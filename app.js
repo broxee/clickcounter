ae275aea36707eae264aa4732f32e354cd96a3f4
Broxee <broxeehq@gmail.com>
2026-03-28 12:36:03 +0530

    Update app.js (random color generator and score indicator)
    here mainly we applied concept of random color generator to get a better experience while clicking the box

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let Box = document.querySelector('.clickBox');
 let score = document.querySelector('h3');
 let timerText = document.querySelector('h2');
 
+
 let count = 0;
 
 // function clickBox(){
@@ -21,11 +22,13 @@ let isGameRunning = true;
 
 let scoreCounter =  Box.addEventListener("click", function(){
     
-    
+    let divColor = colorGenerator();
+    Box.style.backgroundColor = divColor;
     if(isGameRunning){
         count++;
-        score.innerText = count;
+        score.innerText = `Score : ${count}`;
     }
+
 });
 
 
@@ -41,6 +44,16 @@ let interval = setInterval(()=>{
 }
 
 },1000)
+
+let colorGenerator = function(){
+    let red = Math.floor(Math.random()*255);
+    let green= Math.floor(Math.random()*255);
+    let blue  = Math.floor(Math.random()*255);
+    
+    let color = `rgb(${red},${green},${blue})`;
+    return color;
+
+};
 
 
 
